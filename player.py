0bbd2c643d86b0ee2a5cf8fc2d8d51817630d9f4
ayushi1411 <bknashi@gmail.com>
2023-01-01 01:13:39 +0530

    add player jump

--- player.py
+++ player.py
@@ -8,24 +8,37 @@ class Player():
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
 
     def update(self, screen):
         
         dx = 0
         dy = 0
-
         #get keypresses
         key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
         if key[pygame.K_LEFT]:
             dx -= 5
         if key[pygame.K_RIGHT]:
             dx += 5
         
+        #add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
         #check for collision
 
         #update player coordinates
         self.rect.x += dx
         self.rect.y += dy
         
+        if self.rect.bottom > screen.height:
+            self.rect.bottom = screen.height
         #draw player onto the screen
         screen.screen.blit(self.image, self.rect)
