a4567e37554f660f4ffe7e7fc31812e9ac741b80
ayushi1411 <bknashi@gmail.com>
2023-01-27 00:07:42 +0530

    add start menu

--- game.py
+++ game.py
@@ -17,11 +17,14 @@ pygame.display.set_caption('Platformer')
 
 #define game variables
 game_over = 0
+main_menu = True
 
 #load images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 restart_img = pygame.image.load('img/restart_btn.png')
+start_img = pygame.image.load('img/start_btn.png')
+exit_img = pygame.image.load('img/exit_btn.png')
 
 def draw_grid():
     for line in range(0, 20):
@@ -60,6 +63,8 @@ lava_group = pygame.sprite.Group()
 world = World(world_data, screen, blob_group, lava_group)
 
 restart_button = Button(screen.width // 2 - 50, screen.height // 2 + 100, restart_img)
+start_button = Button(screen.width // 2 - 350, screen_height //2, start_img)
+exit_button = Button(screen.width // 2 + 150, screen_height //2, exit_img)
 
 run = True
 while run:
@@ -67,21 +72,27 @@ while run:
     screen.screen.blit(bg_img, (0,0))
     screen.screen.blit(sun_img, (100,100))
 
-    world.draw(screen)
+    if main_menu == True:
+        if exit_button.draw(screen):
+            run = False
+        if start_button.draw(screen):
+            main_menu = False
+    else : 
+        world.draw(screen)
 
-    if game_over == 0:
-        blob_group.update()
+        if game_over == 0:
+            blob_group.update()
 
-    blob_group.draw(screen.screen)
-    lava_group.draw(screen.screen)
+        blob_group.draw(screen.screen)
+        lava_group.draw(screen.screen)
 
-    game_over = player.update(screen, world, blob_group, lava_group, game_over)
+        game_over = player.update(screen, world, blob_group, lava_group, game_over)
 
-    #if player has died
-    if game_over == -1:
-        if restart_button.draw(screen):
-            player.reset(100, screen.height - 130)
-            game_over = 0
+        #if player has died
+        if game_over == -1:
+            if restart_button.draw(screen):
+                player.reset(100, screen.height - 130)
+                game_over = 0
 
 
     for event in pygame.event.get():
